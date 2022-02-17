d43af940ebdf870606679742487394fae72711ee
Steve <sholtebeck@gmail.com>
2022-02-17 08:24:26 -1000

    add routine to save guesses

--- wordle.py
+++ wordle.py
@@ -1,12 +1,14 @@
-import random
+import random, datetime
 wordlines=open('wordle5.txt').readlines()
-commonwords=wordlines[0] 
-allwords=wordlines[1]      
-counts={w:commonwords.count(w) for w in set(commonwords)}
-#wordlist=new_wordlist(allwords,[])
+commonwords=wordlines[0].strip() 
+uncommonwords=wordlines[1].strip()      
+wordlist=[commonwords[x:x+5] for x in range(0,len(commonwords),5) if len(set(commonwords[x:x+5]))==5]
+letters=[''.join(w[i] for w in wordlist) for i in range(5)]
+percent={w:round(commonwords.count(w)/len(commonwords),3) for w in set(commonwords)}
+counts={w:[letters[i].count(w) for i in range(5)] for w in percent.keys()}
 
 def wordscore(word):
-    return sum(counts[w] for w in set(word))
+    return sum(percent[w] for w in set(word))
 
 def random_word(words=commonwords):
     wordlist=new_wordlist(words)  
@@ -33,10 +35,32 @@ def new_wordlist(words=commonwords,guesses=[]):
                 wordlist=[w for w in wordlist if word[x] in w and word[x]!=w[x]]
             elif guess[x]=='R':
                 wordlist=[w for w in wordlist if word[x] not in w]
-    if len(wordlist)>0 or words==allwords:
+            elif guess[x]=='-':
+                wordlist=[w for w in wordlist if len(w)==len(set(w))]
+    if len(wordlist)>0 or words==uncommonwords:
         return wordlist
     else:
-        return new_wordlist(allwords,guesses)
+        return new_wordlist(uncommonwords,guesses)
+
+# save the latest guesses 
+def save_guesses(guesses):
+    lines=open('prevwords.txt').readlines()
+    todate=datetime.date.today().strftime("%m/%d")     
+    word=guesses[-1][0]
+    line=todate+" - #"+str(len(lines))+"("+str(len(guesses))+"): "+word+"\n"
+    o=open('wordlog.txt','a')
+    p=o.write(line)
+    o.close()
+    cwords=new_wordlist(commonwords)
+    uwords=new_wordlist(uncommonwords)
+    if word in cwords:
+        cwords.remove(word)
+        uwords.append(word)
+        uwords.sort()
+        o=open("wordle5.txt","w")
+        p=o.write(''.join(cwords)+"\n")
+        q=o.write(''.join(uwords))
+        o.close()      
 
 def play_wordle():
     wordlist,guesses,found=new_wordlist(commonwords),[],False
@@ -57,7 +81,8 @@ def play_wordle():
         wordlist=new_wordlist(commonwords,guesses)
         wordlist.sort(key=wordscore)
     if found:
-        print("found",guesses[-1][0],"in",len(guesses),'guesses')        
+        print("found",guesses[-1][0],"in",len(guesses),'guesses')
+        save_guesses(guesses)        
             
 print("Let's Play WORDLE") 
 play_wordle()
