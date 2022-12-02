3de19e06223f6ff7af2f687d85b41b014c084b94
Steve <sholtebeck@gmail.com>
2022-12-02 07:05:11 -1000

    12/22 add year to date

--- wordle.py
+++ wordle.py
@@ -47,7 +47,7 @@ def new_wordlist(words=commonwords,guesses=[]):
 # save the latest guesses 
 def save_guesses(guesses):
     lines=open('wordlog.txt').readlines()
-    todate=datetime.date.today().strftime("%m/%d")     
+    todate=datetime.date.today().strftime("%m/%d/%y")     
     word=guesses[-1][0]
     line=todate+" - #"+str(len(lines))+"("+str(len(guesses))+"): "+word+"\n"
     o=open('wordlog.txt','a')
